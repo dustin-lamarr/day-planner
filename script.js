d5b4a9229c96f1ad89f37a836731b745986d2e59
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-10-14 13:34:00 -0600

    Tested function to ensure hour can be compared to hour block div value to set conditional formatting

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 //Sets today's date in body header
-var dateToday = moment().format("MMMM Do YYYY"); 
+var dateToday = moment().format("MMMM Do YYYY");
 console.log(dateToday);
 $('#currentDay').text(dateToday);
 timeCode();
+var timeSlots = ['#nine', '#ten', '#eleven', '#twelve', '#thirteen', '#fourteen', '#fifteen', '#sixteen'];
 
-function timeCode(){
+function timeCode() {
     var currentHour = moment().format('H');
-    var plannerHour = $('#nine-ten').attr('value');
+    // var plannerHour = $('#work-hours').attr('value');
+    
     console.log(plannerHour);
-    if(currentHour > plannerHour){
-        $('#nine-ten').addClass('past')
+    if (currentHour > plannerHour) {
+        $(plannerHour).addClass('past');
+    }
+
+    if (currentHour === plannerHour) {
+        $(planner).addClass('present');
     }
 }
